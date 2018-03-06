72432636cff262a607b3d27d950ad06562f2a961
olblak <me@olblak.com>
2018-03-06 11:14:46 +0100

    Add gunicorn timeout option

--- docker/alpine/entrypoint.sh
+++ docker/alpine/entrypoint.sh
@@ -20,4 +20,4 @@ print(\"Add admin user:\n\tuser: {}\n\temail {}\".format(user,mail))
 
 echo "Hatstall service running on http://localhost:8000"
 # python3 manage.py runserver 0.0.0.0:8000
-gunicorn django_hatstall.wsgi --workers HATSTALL_WORKERS --bind 0.0.0.0:8000
+gunicorn django_hatstall.wsgi --workers "$HATSTALL_GUNICORN_WORKERS" --timeout "$HATSTALL_GUNICORN_TIMEOUT" --bind 0.0.0.0:8000
